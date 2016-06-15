f9dd4078df7310fc88666d16e4508fa9f6dcc129
Ionut Staicu <github@iamntz.com>
2016-06-15 20:56:02 +0300

    Refactored Form Ajax Controller
    Reduced the size of the main method by extracting all stuff into their own methods
    
    Solved conflicts

--- includes/AJAX/Controllers/Form.php
+++ includes/AJAX/Controllers/Form.php
@@ -19,6 +19,43 @@ class NF_AJAX_Controllers_Form extends NF_Abstracts_Controller
 
         $form_data = json_decode( stripslashes( $_POST['form'] ), ARRAY_A );
 
+        $form = $this->get_form( $form_data );
+
+        $form->update_settings( $form_data[ 'settings' ] )->save();
+
+        if( isset( $form_data[ 'fields' ] ) ) {
+            $this->parse_form_fields( $form_data );
+        }
+
+        if( isset( $form_data[ 'deleted_fields' ] ) ){
+            $this->parse_deleted_fields( $form_data );
+        }
+
+        if( isset( $form_data[ 'actions' ] ) ) {
+            $this->parse_actions( $form_data );
+        }
+
+        $this->parse_actions2( $form_data );
+
+        if( isset( $form_data[ 'deleted_actions' ] ) ){
+            $this->parse_deleted_actions( $form_data );
+        }
+
+        delete_user_option( get_current_user_id(), 'nf_form_preview_' . $form_data['id'] );
+        do_action( 'ninja_forms_save_form', $form->get_id() );
+
+        $this->_respond();
+    }
+
+    public function delete()
+    {
+        check_ajax_referer( 'ninja_forms_builder_nonce', 'security' );
+
+        $this->_respond();
+    }
+
+    protected function get_form( $form_data )
+    {
         if( is_string( $form_data[ 'id' ] ) ) {
             $tmp_id = $form_data[ 'id' ];
             $form = Ninja_Forms()->form()->get();
@@ -29,74 +66,78 @@ class NF_AJAX_Controllers_Form extends NF_Abstracts_Controller
             $form = Ninja_Forms()->form($form_data['id'])->get();
         }
 
-        $form->update_settings( $form_data[ 'settings' ] )->save();
+        return $form;;
+    }
 
-        if( isset( $form_data[ 'fields' ] ) ) {
-            foreach ($form_data['fields'] as $field_data) {
+    protected function parse_form_fields( $form_data )
+    {
+        foreach ($form_data['fields'] as $field_data) {
 
-                $id = $field_data['id'];
+            $id = $field_data['id'];
 
-                $field = Ninja_Forms()->form( $form_data[ 'id' ] )->get_field($id);
+            $field = Ninja_Forms()->form( $form_data[ 'id' ] )->get_field($id);
 
-                $field->update_settings($field_data['settings'])->save();
+            $field->update_settings($field_data['settings'])->save();
 
-                if ($field->get_tmp_id()) {
+            if ($field->get_tmp_id()) {
 
-                    $tmp_id = $field->get_tmp_id();
-                    $this->_data['new_ids']['fields'][$tmp_id] = $field->get_id();
-                }
-
-                $this->_data[ 'fields' ][ $id ] = $field->get_settings();
+                $tmp_id = $field->get_tmp_id();
+                $this->_data['new_ids']['fields'][$tmp_id] = $field->get_id();
             }
+
+            $this->_data[ 'fields' ][ $id ] = $field->get_settings();
         }
+    }
 
-        if( isset( $form_data[ 'deleted_fields' ] ) ){
+    protected function parse_deleted_fields( $form_data )
+    {
+        foreach( $form_data[ 'deleted_fields' ] as  $deleted_field_id ){
 
-            foreach( $form_data[ 'deleted_fields' ] as  $deleted_field_id ){
-
-                $field = Ninja_Forms()->form()->get_field( $deleted_field_id );
-                $field->delete();
-            }
+            $field = Ninja_Forms()->form()->get_field( $deleted_field_id );
+            $field->delete();
         }
+    }
 
-        if( isset( $form_data[ 'actions' ] ) ) {
 
-            /*
-             * Loop Actions and fire Save() hooks.
-             */
-            foreach ($form_data['actions'] as $action_data) {
+    /*
+     * Loop Actions and fire Save() hooks.
+     */
+    protected function parse_actions( $form_data )
+    {
+        foreach ($form_data['actions'] as $action_data) {
 
-                $id = $action_data['id'];
+            $id = $action_data['id'];
 
-                $action = Ninja_Forms()->form( $form_data[ 'id' ] )->get_action( $id );
+            $action = Ninja_Forms()->form( $form_data[ 'id' ] )->get_action( $id );
 
-                $action->update_settings($action_data['settings'])->save();
+            $action->update_settings($action_data['settings'])->save();
 
-                $action_type = $action->get_setting( 'type' );
+            $action_type = $action->get_setting( 'type' );
 
-                if( isset( Ninja_Forms()->actions[ $action_type ] ) ) {
-                    $action_class = Ninja_Forms()->actions[ $action_type ];
+            if( isset( Ninja_Forms()->actions[ $action_type ] ) ) {
+                $action_class = Ninja_Forms()->actions[ $action_type ];
 
-                    $action_settings = $action_class->save( $action_data['settings'] );
-                    if( $action_settings ){
-                        $action_data['settings'] = $action_settings;
-                        $action->update_settings( $action_settings )->save();
-                    }
+                $action_settings = $action_class->save( $action_data['settings'] );
+                if( $action_settings ){
+                    $action_data['settings'] = $action_settings;
+                    $action->update_settings( $action_settings )->save();
                 }
-
-                if ($action->get_tmp_id()) {
-
-                    $tmp_id = $action->get_tmp_id();
-                    $this->_data['new_ids']['actions'][$tmp_id] = $action->get_id();
-                }
-
-                $this->_data[ 'actions' ][ $id ] = $action->get_settings();
             }
-        }
 
-        /*
-         * Loop Actions and fire Publish() hooks.
-         */
+            if ($action->get_tmp_id()) {
+                $tmp_id = $action->get_tmp_id();
+                $this->_data['new_ids']['actions'][$tmp_id] = $action->get_id();
+            }
+
+            $this->_data[ 'actions' ][ $id ] = $action->get_settings();
+        }
+    }
+
+    /*
+     * Loop Actions and fire Publish() hooks.
+     */
+    protected function parse_actions2( $form_data )
+    {
         foreach ($form_data['actions'] as $action_data) {
 
             $action = Ninja_Forms()->form( $form_data[ 'id' ] )->get_action( $action_data['id'] );
@@ -114,27 +155,14 @@ class NF_AJAX_Controllers_Form extends NF_Abstracts_Controller
                 }
             }
         }
-
-        if( isset( $form_data[ 'deleted_actions' ] ) ){
-
-            foreach( $form_data[ 'deleted_actions' ] as  $deleted_action_id ){
-
-                $action = Ninja_Forms()->form()->get_action( $deleted_action_id );
-                $action->delete();
-            }
-        }
-
-        delete_user_option( get_current_user_id(), 'nf_form_preview_' . $form_data['id'] );
-
-        do_action( 'ninja_forms_save_form', $form->get_id() );
-
-        $this->_respond();
     }
 
-    public function delete()
+    protected function parse_deleted_actions( $form_data )
     {
-        check_ajax_referer( 'ninja_forms_builder_nonce', 'security' );
+        foreach( $form_data[ 'deleted_actions' ] as  $deleted_action_id ){
 
-        $this->_respond();
+            $action = Ninja_Forms()->form()->get_action( $deleted_action_id );
+            $action->delete();
+        }
     }
 }
